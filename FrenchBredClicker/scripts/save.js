d29ef47957288d5576ad955244300c54782eaf40
spzla <patryk2k5.9909@gmail.com>
2021-06-22 17:31:08 +0200

    reset counter at save deletion

--- FrenchBredClicker/scripts/save.js
+++ FrenchBredClicker/scripts/save.js
@@ -5,5 +5,6 @@ delSaveButton.addEventListener('click', () => {
   if(!confirm('Do you want to reset your save?')) { return alert('Your save was not reset!'); }
   document.cookie = `points=0`;
   document.cookie = `pointsPerClick=1`;
+  counter.innerHTML = `French Breds:<br>${getCookieValue('points')}`;
   alert('Your save was successfully reset!');
 });
