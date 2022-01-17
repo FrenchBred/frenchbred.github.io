668b1240195ec312a21fc94f8d0d1364e43a845e
Lisu <ruzia12349@wp.pl>
2022-01-17 15:38:03 +0100

    v2

--- burger.js
+++
@@ -1,8 +0,0 @@
-let burgers = document.querySelectorAll('.burger');
-
-[].forEach.call(burgers, burger => {
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('collapsed');
-    burger.parentNode.classList.toggle('open');
-  });
-});
